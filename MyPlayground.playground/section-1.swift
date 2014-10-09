c7a1515486559ba6f50cf9110f5f2f8f164304a7
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-09 15:12:51 -0700

    Updated Swift Playground with some Harry Potter code

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -11,3 +11,10 @@ for var i = 0; i <= 10; i++ {
 }
 
 sum
+
+let a = 3
+let b = 5
+var streetname = "Privet Drive"
+let uppercaseStreetName = streetname.uppercaseString
+let familyDescription = "Terrible"
+let fullSentence = "HARRY POTTER LIVES ON \(a + b) \(uppercaseStreetName) WITH HIS \(familyDescription) FAMILY."
